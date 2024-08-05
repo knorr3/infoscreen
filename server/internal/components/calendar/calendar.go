15c64f3a0a3454e49f9b8f9dc06b38216f7e60b5
Adrian Knorr <adrian.knorr@consol.de>
2024-08-05 15:15:31 +0200

    Rename and implement calendar

---
+++ server/internal/components/calendar/calendar.go
@@ -0,0 +1,51 @@
+package calendar
+
+import (
+	"bytes"
+	"time"
+
+	"github.com/apognu/gocal"
+	util "github.com/knorr3/infoscreen/server/internal/util"
+)
+
+type Event struct {
+	Date  time.Time `json:"date"`
+	Title string    `json:"title"`
+}
+
+func GetData(limit int) (events []Event, err error) {
+	url, err := util.GetEnv("ICAL_URL", "")
+	if err != nil {
+		return
+	}
+
+	body, err := util.MakeAPIRequest(url, "")
+	if err != nil {
+		return
+	}
+
+	ics := bytes.NewReader(body)
+
+	// Two weeks
+	start, end := time.Now(), time.Now().Add(2*7*24*time.Hour)
+
+	c := gocal.NewParser(ics)
+	c.Start, c.End = &start, &end
+	err = c.Parse()
+	if err != nil {
+		return
+	}
+
+	for idx, event := range c.Events {
+		if idx >= limit {
+			return
+		}
+
+		events = append(events, Event{
+			Date:  *event.Start,
+			Title: event.Summary,
+		})
+	}
+
+	return
+}
