becd15757a9d857a9660a4139f7e41563dedfeb4
Adrian Knorr <adrian.knorr@consol.de>
2024-08-05 15:26:59 +0200

    Provide example env

---
+++ server/demo-env.sh
@@ -0,0 +1,16 @@
+# MQTT
+BROKER_IP=192.168.0.123
+BROKER_USERNAME=mqtt
+BROKER_PASSWORD=password
+
+# MVV
+MVV_STATION=de:09178:3110
+
+# Weather
+WEATHER_USERNAME=user
+WEATHER_PASSWORD=password
+WEATHER_LAT=50.7753
+WEATHER_LONG=6.0839
+
+# Calendar
+ICAL_URL=https://calendar.google.com/calendar/ical/anyrandomsubdomaingooglecanthinkof.ical
