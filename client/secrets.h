a423b2d91b44f1428834a9bf8056fc2157dd3e1e
knorr3 <adrian.f.knorr@gmail.com>
2025-02-07 20:46:08 +0100

    Add client dependencies
    * Weather icons
    * Sleep picture (pillow)
    * Secrets
    * Fonts

---
+++ client/secrets.h
@@ -0,0 +1,14 @@
+#ifndef WIFI_SSID
+#define WIFI_SSID "ssid"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "password"
+#endif
+
+const char *mqtt_server = "192.168.0.1";
+const int mqtt_port = 1883;
+const char *mqtt_username = "username";
+const char *mqtt_password = "password";
+const char *mqtt_pub_topic = "infoscreen/question";
+const char *mqtt_sub_topic = "infoscreen/answer";
