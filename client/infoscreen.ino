7aefd4f7a480f37aeb5d6072ebd22636e6ef0294
knorr3 <adrian.f.knorr@gmail.com>
2025-02-07 20:44:10 +0100

    Big client update :)

--- client/infoscreen.ino
+++ client/infoscreen.ino
@@ -23,7 +23,12 @@
 #include "epd_driver.h"
 #include "firasans.h"
 #include "courb36.h"
+#include "courb28.h"
+#include "courb20.h"
+#include "cour28.h"
+#include "cour24.h"
 #include "cour20.h"
+#include "courb16.h"
 #include "cour16.h"
 #include "esp_adc_cal.h"
 #include <FS.h>
@@ -39,6 +44,8 @@
 #include "pillow.h"
 #include <PubSubClient.h>
 #include "secrets.h"
+#include <ArduinoJson.h>
+#include "icons.h"
 
 /////////////////// WIFI //////////////////////////
 // #ifndef WIFI_SSID
@@ -59,7 +66,8 @@
 
 unsigned long lastMsg = 0;
 #define MQTT_BUFFER_SIZE (16384)
-char msg[MQTT_BUFFER_SIZE];
+#define MISC_MSG_SIZE (1024)
+char msg[MISC_MSG_SIZE];
 
 /////////////////// TIME //////////////////////////
 const char *ntpServer1 = "pool.ntp.org";
@@ -78,12 +86,16 @@ PubSubClient client(espClient);
 /////////////////// VARS //////////////////////////
 uint8_t *framebuffer = NULL;
 bool touchOnline = false;
-uint32_t big_interval = 30000; // 30sec
+// uint32_t big_interval = 30000; // 30sec
+uint32_t big_interval = 60000; // 1min
+// uint32_t big_interval = 300000; // 5min
 uint32_t small_interval = 300;
 uint32_t big_interval_timestamp = 0;
 uint32_t small_interval_timestamp = 0;
 int vref = 1100;
 char buf[128];
+uint32_t pressed_cnt = 0;
+uint16_t touch_registered = 0;
 int16_t last_x = 0;
 int16_t last_y = 0;
 
@@ -102,6 +114,41 @@ struct _point
     {4, EPD_WIDTH / 2 - 60, EPD_HEIGHT - 80, 120, 80},
     {5, EPD_WIDTH / 2 - 60, 10, 120, 80}};
 
+const uint8_t *weather_icons[] = {
+    // Unkown
+    w0_data,
+    // Day
+    w1_data, w2_data, w3_data, w4_data, w5_data, w6_data, w7_data, w8_data, w9_data, w10_data, w11_data, w12_data, w13_data, w14_data, w15_data, w16_data,
+    // Night
+    w101_data, w102_data, w103_data, w104_data, w105_data, w106_data, w107_data, w108_data, w109_data, w110_data, w111_data, w112_data, w113_data, w114_data, w115_data, w116_data};
+
+#define MAX_WEATHER_DATA 3
+struct weather_data
+{
+    char date[32];
+    float temp;
+    float precip;
+    uint8_t icon_idx;
+} weather[MAX_WEATHER_DATA];
+
+#define MAX_DB_DATA 5
+struct db_data
+{
+    char scheduledDeparture[32];
+    char destination[128];
+    char train[12];
+    int delayDeparture;
+    int isCancelled;
+} db[MAX_DB_DATA];
+
+#define MAX_CAL_DATA 5
+struct cal_data
+{
+    char date[32];
+    char time[32];
+    char title[128];
+} cal[MAX_CAL_DATA];
+
 /////////////////// DEVICE FUNCTIONS //////////////////////////
 void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info)
 {
@@ -116,7 +163,6 @@ void timeavailable(struct timeval *t)
     rtc.hwClockWrite();
 }
 
-uint32_t pressed_cnt = 0;
 void buttonPressed(Button2 &b)
 {
     Serial.println("Button1 Pressed!");
@@ -138,36 +184,139 @@ void buttonPressed(Button2 &b)
     epd_poweroff_all();
 }
 
-void mqtt_rec(char *topic, byte *payload, unsigned int length) {
-    Serial.printf("Message arrived in topic: %s\n", topic);
-    Serial.printf("Message (%d):", length);
-    Serial.write(payload, length);
-    Serial.println();
-    Serial.println("-----------------------");
+void mqtt_rec(char *topic, byte *payload, unsigned int length)
+{
+    // Serial.printf("Message arrived in topic: %s\n", topic);
+    // Serial.printf("Message (%d):", length);
+    // Serial.write(payload, length);
+    // Serial.println();
+    // Serial.println("-----------------------");
+
+    // Parse the JSON message
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+    {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        deep_sleep("JSON parse error");
+    }
+
+    // Extract the values
+    JsonArray entries = doc["entries"];
+    for (JsonVariant entry : entries)
+    {
+        if (entry["type"] == "weather" && entry["status"] == "ok")
+        {
+            JsonArray data = entry["data"];
+            int i = 0;
+            for (JsonVariant item : data)
+            {
+                if (i >= MAX_WEATHER_DATA)
+                {
+                    break;
+                }
+                strncpy(weather[i].date, item["date"], sizeof(weather[i].date));
+                weather[i].temp = item["temp"];
+                weather[i].precip = item["precip"];
+                weather[i].icon_idx = item["icon-idx"];
+                i++;
+            }
+        }
+
+        if (entry["type"] == "db" && entry["status"] == "ok")
+        {
+            JsonArray data = entry["data"];
+            int i = 0;
+            for (JsonVariant item : data)
+            {
+                if (i >= MAX_DB_DATA)
+                {
+                    break;
+                }
+                strncpy(db[i].scheduledDeparture, item["scheduledDeparture"], sizeof(db[i].scheduledDeparture));
+                strncpy(db[i].destination, item["destination"], sizeof(db[i].destination));
+                strncpy(db[i].train, item["train"], sizeof(db[i].train));
+                db[i].delayDeparture = item["delayDeparture"];
+                db[i].isCancelled = item["isCancelled"];
+                i++;
+            }
+        }
+
+        if (entry["type"] == "calendar" && entry["status"] == "ok")
+        {
+            JsonArray data = entry["data"];
+            int i = 0;
+            for (JsonVariant item : data)
+            {
+                if (i >= MAX_CAL_DATA)
+                {
+                    break;
+                }
+                strncpy(cal[i].date, item["date"], sizeof(cal[i].date));
+                // strncpy(cal[i].time, item["time"], sizeof(cal[i].time));
+                strncpy(cal[i].title, item["title"], sizeof(cal[i].title));
+                i++;
+            }
+        }
+    }
+
+    Serial.println("Weather data:");
+    for (int i = 0; i < MAX_WEATHER_DATA; i++)
+    {
+        Serial.printf("Date: %s, Temp: %.1f, Precip: %.1f, Icon: %d\n",
+                      weather[i].date, weather[i].temp, weather[i].precip, weather[i].icon_idx);
+    }
+
+    Serial.println("DB data:");
+    for (int i = 0; i < MAX_DB_DATA; i++)
+    {
+        Serial.printf("Scheduled Departure: %s, Destination: %s, Train: %s, Delay: %d, Cancelled: %d\n",
+                      db[i].scheduledDeparture, db[i].destination, db[i].train, db[i].delayDeparture, db[i].isCancelled);
+    }
+
+    Serial.println("Calendar data:");
+    for (int i = 0; i < MAX_CAL_DATA; i++)
+    {
+        Serial.printf("Date: %s, Title: %s\n",
+                      cal[i].date, cal[i].title);
+    }
+
+    draw_layout();
+    draw_rtc_time();
+    draw_rtc_date();
+    draw_weather();
+    draw_db();
+    draw_cal();
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(mqtt_pub_topic, "Reconnected");
-      // ... and resubscribe
-      client.subscribe(mqtt_sub_topic);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+void reconnect()
+{
+    // Loop until we're reconnected
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        // Attempt to connect
+        if (client.connect(clientId.c_str()))
+        {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            client.publish(mqtt_pub_topic, "Reconnected");
+            // ... and resubscribe
+            client.subscribe(mqtt_sub_topic);
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
     }
-  }
 }
 
 /////////////////// DISPLAY FUNCTIONS //////////////////////////
@@ -321,6 +470,166 @@ void draw_rtc_date()
     epd_poweroff_all();
 }
 
+void draw_weather()
+{
+    epd_poweron();
+
+    Rect_t area = {
+        .x = 400,
+        .y = 80,
+        .width = 64,
+        .height = 64,
+    };
+    int32_t cursor_x;
+    int32_t cursor_y;
+    int32_t icon_idx;
+
+    // Loop over the weather data
+    for (int i = 0; i < MAX_WEATHER_DATA; i++)
+    {
+        int temp_int = (int)round(weather[i].temp);
+        snprintf(buf, 128, "%d°C",temp_int); 
+        int temp_length = strlen(buf) - 3;
+
+        cursor_x = 400 - 5*temp_length  + i*205;
+        cursor_y = 70;
+        area.x = 400 + i * 200;
+        writeln((GFXfont *)&CourB28, buf, &cursor_x, &cursor_y, NULL);
+
+        icon_idx = weather[i].icon_idx;
+        if (icon_idx > 16) { icon_idx = icon_idx - 100 + 16; }
+        epd_draw_grayscale_image(area, (uint8_t *)weather_icons[icon_idx]);
+        epd_draw_image(area, (uint8_t *)weather_icons[icon_idx], BLACK_ON_WHITE);
+    } 
+    epd_poweroff_all();
+}
+
+void draw_db()
+{
+    epd_poweron();
+
+    int32_t cursor_x;
+    int32_t cursor_y;
+
+    // Loop over the db data
+    // Three rows, Cour24
+    // for (int i = 0; i < MAX_DB_DATA; i++)
+    // {
+    //     cursor_x = 30;
+    //     cursor_y = 200 + i*130;
+
+    //     snprintf(buf, 128, "%s", db[i].train);
+    //     writeln((GFXfont *)&CourB28, buf, &cursor_x, &cursor_y, NULL);
+        
+    //     cursor_x = 160;
+    //     snprintf(buf, 128, "%s", db[i].scheduledDeparture);
+    //     writeln((GFXfont *)&Cour28, buf, &cursor_x, &cursor_y, NULL);
+
+        
+    //     cursor_x = 390;
+    //     if (db[i].delayDeparture > 0)
+    //     {
+    //         if (db[i].delayDeparture > 9) { cursor_x -= 20; }
+    //         snprintf(buf, 128, "+%d", db[i].delayDeparture); 
+    //         writeln((GFXfont *)&CourB28, buf, &cursor_x, &cursor_y, NULL);
+    //     }
+    //     else if (db[i].isCancelled)
+    //     {
+    //         snprintf(buf, 128, "X!");
+    //         writeln((GFXfont *)&CourB28, buf, &cursor_x, &cursor_y, NULL);
+    //     }
+    //     else
+    //     {
+    //         snprintf(buf, 128, "OK");
+    //         writeln((GFXfont *)&Cour28, buf, &cursor_x, &cursor_y, NULL);
+    //     }
+
+    //     cursor_x = 30;
+    //     cursor_y += 50;
+    //     // If the destination is too long, cut it off
+    //     snprintf(buf, 128, "%s", db[i].destination);
+    //     buf[12] = '\0';
+    //     writeln((GFXfont *)&Cour24, buf, &cursor_x, &cursor_y, NULL);
+    // } 
+
+    // 6 rows, Cour20
+    for (int i = 0; i < MAX_DB_DATA; i++)
+    {
+        cursor_x = 20;
+        cursor_y = 200 + i*70;
+
+        snprintf(buf, 128, "%s", db[i].scheduledDeparture);
+        writeln((GFXfont *)&Cour20, buf, &cursor_x, &cursor_y, NULL);
+        
+        cursor_x = 170;
+        if (db[i].isCancelled)
+        {
+            snprintf(buf, 128, "--");
+            writeln((GFXfont *)&CourB20, buf, &cursor_x, &cursor_y, NULL);
+        }
+        else if (db[i].delayDeparture > 0)
+        {
+            if (db[i].delayDeparture > 9) { cursor_x -= 20; }
+            snprintf(buf, 128, "+%d", db[i].delayDeparture); 
+            writeln((GFXfont *)&CourB20, buf, &cursor_x, &cursor_y, NULL);
+        }
+        else
+        {
+            snprintf(buf, 128, "+0");
+            writeln((GFXfont *)&Cour20, buf, &cursor_x, &cursor_y, NULL);
+        }
+
+        cursor_x = 230;
+        // If the destination is too long, cut it off
+        snprintf(buf, 128, "%s", db[i].destination);
+        buf[9] = '\0';
+        writeln((GFXfont *)&Cour20, buf, &cursor_x, &cursor_y, NULL);
+
+        if (db[i].isCancelled)
+        {
+            cursor_x = 20;
+            snprintf(buf, 128, "------------------");
+            writeln((GFXfont *)&Cour20, buf, &cursor_x, &cursor_y, NULL);
+        }
+    } 
+    epd_poweroff_all();
+}
+
+void draw_cal()
+{
+    epd_poweron();
+
+    int32_t cursor_x;
+    int32_t cursor_y;
+
+    for (int i = 0; i < MAX_CAL_DATA; i++)
+    {
+        cursor_x = 500;
+        cursor_y = 200 + i*125;
+
+        String isoDate = cal[i].date;
+
+        snprintf(buf, 128, "%s.%s %s:%s", 
+            isoDate.substring(8, 10), //Day
+            isoDate.substring(5, 7), //Month
+            isoDate.substring(11, 13), //Hour
+            isoDate.substring(14, 16) //Minute
+            
+        );
+        // snprintf(buf, 128, "%s", cal[i].date);
+        writeln((GFXfont *)&Cour20, buf, &cursor_x, &cursor_y, NULL);
+        
+        cursor_x = 500;
+        cursor_y += 50;
+        // If the title is too long, cut it off
+        snprintf(buf, 128, "%s", cal[i].title);
+        buf[18] = '\0';
+        writeln((GFXfont *)&CourB20, buf, &cursor_x, &cursor_y, NULL);
+    } 
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    epd_poweroff_all();
+}
+
 void draw_layout()
 {
     epd_poweron();
@@ -331,8 +640,18 @@ void draw_layout()
     delay(10);
 
     epd_draw_hline(0, 144, EPD_WIDTH, 0, framebuffer);
-    epd_draw_vline(320, 0, EPD_HEIGHT, 0, framebuffer);
-    epd_draw_vline(640, 144, EPD_HEIGHT - 144, 0, framebuffer);
+
+    // Three column layout
+    // epd_draw_vline(320, 0, EPD_HEIGHT, 0, framebuffer);
+    // epd_draw_vline(640, 144, EPD_HEIGHT - 144, 0, framebuffer);
+
+    // Two column layout
+    epd_draw_vline(320, 0, 144, 0, framebuffer);
+    epd_draw_vline(EPD_WIDTH/2, 144, EPD_HEIGHT - 144, 0, framebuffer);
+    
+    // Debug Weather
+    // epd_draw_vline(533, 0, 144, 0, framebuffer);
+    // epd_draw_vline(747, 0, 144, 0, framebuffer);
     epd_draw_grayscale_image(epd_full_screen(), framebuffer);
     epd_poweroff_all();
 }
@@ -375,9 +694,12 @@ void setup()
             deep_sleep("MQTT probe failed!!!");
         }
     }
-    client.publish(mqtt_pub_topic, "Hi, I'm ESP32 :)");
     client.subscribe(mqtt_sub_topic);
 
+    // Send out first request
+    sniprintf(msg, MISC_MSG_SIZE, "{\"type\":\"request\",\"data\":[{\"name\":\"weather\"},{\"name\":\"db\",\"count\":%d},{\"name\":\"calendar\",\"count\":%d}]}", MAX_DB_DATA, MAX_CAL_DATA);
+    client.publish(mqtt_pub_topic, msg);
+    
     // Time
     sntp_set_time_sync_notification_cb(timeavailable);
     configTzTime(time_zone, ntpServer1, ntpServer2);
@@ -412,11 +734,13 @@ void setup()
     // Touch
     uint8_t touchAddress = 0x00;
     Wire.beginTransmission(0x14);
-    if (Wire.endTransmission() == 0) {
+    if (Wire.endTransmission() == 0)
+    {
         touchAddress = 0x14;
     }
     Wire.beginTransmission(0x5D);
-    if (Wire.endTransmission() == 0) {
+    if (Wire.endTransmission() == 0)
+    {
         touchAddress = 0x5D;
     }
     if (touchAddress == 0x00)
@@ -441,29 +765,43 @@ void setup()
 
     // Set the button callback function
     btn.setPressedHandler(buttonPressed);
+
+    client.loop();
 }
 
 /////////////////// LOOP //////////////////////////
 void loop()
 {
     // MQTT
-    if (!client.connected()) {
+    if (!client.connected())
+    {
         reconnect();
     }
     client.loop();
 
     // Big Interval
-    if (millis() > big_interval_timestamp)
+    if (millis() > big_interval_timestamp || touch_registered)
     {
-        // Every 10 seconds
+        // Every big_interval
         big_interval_timestamp = millis() + big_interval;
 
-        draw_layout();
-        draw_rtc_time();
-        draw_rtc_date();
+        // Reset touch
+        touch_registered = 0;
+
+        // client.publish(mqtt_pub_topic, "{\"type\":\"request\",\"data\":[\"weather\",\"db\"]}");
+        sniprintf(msg, MISC_MSG_SIZE, "{\"type\":\"request\",\"data\":[{\"name\":\"weather\"},{\"name\":\"db\",\"count\":%d},{\"name\":\"calendar\",\"count\":%d}]}", MAX_DB_DATA, MAX_CAL_DATA);
+        client.publish(mqtt_pub_topic, msg);
+
+
+        // draw_layout();
+        // draw_rtc_time();
+        // draw_rtc_date();
+        // draw_weather();
+        // draw_db();
+        // draw_cal();
         // draw_touch_buttons();
 
-        Serial.println("big loop");
+        // Serial.println("big loop");
         // delay(1000);
     }
 
@@ -489,49 +827,49 @@ void loop()
     }
     last_x = x;
     last_y = y;
+    touch_registered = 1;
 
-    epd_poweron();
+    // epd_poweron();
 
-    int32_t cursor_x = 200;
-    int32_t cursor_y = 450;
+    // int32_t cursor_x = 200;
+    // int32_t cursor_y = 450;
 
-    Rect_t area = {
-        .x = 200,
-        .y = 410,
-        .width = 400,
-        .height = 50,
-    };
-    epd_clear_area(area);
+    // Rect_t area = {
+    //     .x = 200,
+    //     .y = 410,
+    //     .width = 400,
+    //     .height = 50,
+    // };
+    // epd_clear_area(area);
 
-    snprintf(buf, 128, "➸ X:%d Y:%d", x, y);
-    Serial.println(buf);
+    // snprintf(buf, 128, "➸ X:%d Y:%d", x, y);
+    // Serial.println(buf);
 
-    bool pressButton = false;
-    for (int i = 0; i < sizeof(touchPoint) / sizeof(touchPoint[0]); ++i)
-    {
-        if ((x > touchPoint[i].x && x < (touchPoint[i].x + touchPoint[i].w)) && (y > touchPoint[i].y && y < (touchPoint[i].y + touchPoint[i].h)))
-        {
-            snprintf(buf, 128, "➸ Pressed Button: %c\n", 65 + touchPoint[i].buttonID);
-            Serial.println(buf);
-            writeln((GFXfont *)&FiraSans, buf, &cursor_x, &cursor_y, NULL);
-            pressButton = true;
+    // bool pressButton = false;
+    // for (int i = 0; i < sizeof(touchPoint) / sizeof(touchPoint[0]); ++i)
+    // {
+    //     if ((x > touchPoint[i].x && x < (touchPoint[i].x + touchPoint[i].w)) && (y > touchPoint[i].y && y < (touchPoint[i].y + touchPoint[i].h)))
+    //     {
+    //         snprintf(buf, 128, "➸ Pressed Button: %c\n", 65 + touchPoint[i].buttonID);
+    //         Serial.println(buf);
+    //         // writeln((GFXfont *)&FiraSans, buf, &cursor_x, &cursor_y, NULL);
+    //         pressButton = true;
 
-            if (touchPoint[i].buttonID == 4)
-            {
-                deep_sleep("");
-            }
-            if (touchPoint[i].buttonID == 5)
-            {
-                deep_clean();
-            }
-        }
-    }
-    if (!pressButton)
-    {
-        writeln((GFXfont *)&FiraSans, buf, &cursor_x, &cursor_y, NULL);
-    }
+    //         if (touchPoint[i].buttonID == 4)
+    //         {
+    //             deep_sleep("");
+    //         }
+    //         if (touchPoint[i].buttonID == 5)
+    //         {
+    //             deep_clean();
+    //         }
+    //     }
+    // }
+    // if (!pressButton)
+    // {
+    //     // writeln((GFXfont *)&FiraSans, buf, &cursor_x, &cursor_y, NULL);
+    // }
     epd_poweroff_all();
 
     delay(2);
 }
-
