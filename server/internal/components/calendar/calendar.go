887b2cdbff07171f6b5cd2319e14a54d69d25941
knorr3 <adrian.f.knorr@gmail.com>
2025-03-11 14:38:17 +0100

    Calendar: add all day and today flag

--- server/internal/components/calendar/calendar.go
+++ server/internal/components/calendar/calendar.go
@@ -10,8 +10,10 @@ import (
 )
 
 type Event struct {
-	Date  time.Time `json:"date"`
-	Title string    `json:"title"`
+	Date   time.Time `json:"date"`
+	Title  string    `json:"title"`
+	AllDay bool      `json:"allDay"`
+	Today  bool      `json:"today"`
 }
 
 var url string
@@ -21,11 +23,16 @@ func New() (err error) {
 	return
 }
 
+// TODO Timezone handling
 func GetData(limit int) (events []Event, err error) {
 	body, err := util.MakeAPIRequest(url, "")
 	if err != nil {
 		return
 	}
+
+	// Get the current date to set the "today" field in the response
+	now := time.Now()
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 
 	ics := bytes.NewReader(body)
 
@@ -39,6 +46,11 @@ func GetData(limit int) (events []Event, err error) {
 		return
 	}
 
+	// Sort the events slice by datetime
+	sort.Slice(c.Events, func(i, j int) bool {
+		return c.Events[i].Start.Unix() < c.Events[j].Start.Unix()
+	})
+
 	for idx, event := range c.Events {
 		if idx >= limit {
 			return
@@ -47,13 +59,11 @@ func GetData(limit int) (events []Event, err error) {
 		events = append(events, Event{
 			Date:  *event.Start,
 			Title: event.Summary,
+			// AllDay is true if event lasts from 0 to 23:59
+			AllDay: event.Start.Hour() == 0 && event.Start.Minute() == 0 && event.End.Hour() == 23 && event.End.Minute() == 59,
+			Today:  event.Start.Format("2006-01-02") == today.Format("2006-01-02"),
 		})
 	}
-
-	// Sort the events slice by datetime
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].Date.Unix() < events[j].Date.Unix()
-	})
 
 	return
 }
